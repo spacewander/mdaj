e32c98a46ad79f7c6399334cbacca550c84b19a3
spacewander <spacewanderlzx@gmail.com>
2020-10-31 12:11:17 +0800

    init

---
+++ mdaj.sh
@@ -0,0 +1,34 @@
+m() {
+    case $1 in
+        -a )
+            if [ $# -eq 1 ]; then
+                >&2 echo "Too few arguments"
+            else
+                local dir
+                if [ $# -eq 3 ]; then
+                    dir="$3"
+                else
+                    dir="$(pwd)"
+                fi
+                git config --global --add mdaj."$2" "$dir"
+            fi
+            ;;
+        -l )
+            git config --global --get-regexp 'mdaj.*' | \
+                cut -c 6- # remove common prefix
+            ;;
+        * )
+            if [ $# -eq 0 ]; then
+                >&2 echo "Too few arguments"
+            else
+                local dst
+                dst="$(git config --global --get mdaj."$1")"
+                if [ $? -eq 0 ]; then
+                    cd "$dst"
+                else
+                    >&2 echo "Mark $1 not found"
+                fi
+            fi
+            ;;
+    esac
+}
